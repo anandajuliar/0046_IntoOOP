79623cd125d75c31b9de5682cf6aa71b62a8148a
anandajuliar <anandajulia001@gmail.com>
2024-04-23 19:28:59 +0700

    void output

--- 0046_IntoOOP/0046_IntoOOP.cpp
+++ 0046_IntoOOP/0046_IntoOOP.cpp
@@ -28,5 +28,11 @@ public:
 		cout << "SKS :";
 		cin >> sks;
 	}
+	void output() {
+		cout << "kode MK :" << kodeMk << endl;
+		cout << "Nama MK :" << namaMk << endl;
+		cout << "SKS :" << sks << endl;
+	}
+};
 
 
