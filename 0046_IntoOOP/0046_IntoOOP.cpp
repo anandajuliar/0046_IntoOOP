0f0499976c2834ebb5f886b9e53b949ab201b875
anandajuliar <anandajulia001@gmail.com>
2024-04-23 19:46:20 +0700

    fix it

--- 0046_IntoOOP/0046_IntoOOP.cpp
+++ 0046_IntoOOP/0046_IntoOOP.cpp
@@ -2,37 +2,37 @@
 using namespace std;
 
 class Mahasiswa {
-public: // akses modifier
-	string nama;
-	int umur;
-	string jurusan;
+	public: // akses modifier
+		string nama;
+		int umur;
+		string jurusan;
 
-	void output() {
-		cout << "Nama : " << nama << endl;
-		cout << "Umur : " << umur << endl;
-		cout << "Jurusan : " << jurusan << endl;
-	}
+		void output() {
+			cout << "Nama : " << nama << endl;
+			cout << "Umur : " << umur << endl;
+			cout << "Jurusan : " << jurusan << endl;
+		}
 };
 
 class Matakuliah {
-private:
-	string kodeMk;
-	string namaMk;
-	int sks;
-public:
-	void input() {
-		cout << "Kode MK :";
-		cin >> kodeMk;
-		cout << "Nama MK :";
-		cin >> namaMk;
-		cout << "SKS :";
-		cin >> sks;
-	}
-	void output() {
-		cout << "kode MK :" << kodeMk << endl;
-		cout << "Nama MK :" << namaMk << endl;
-		cout << "SKS :" << sks << endl;
-	}
+	private:
+		string kodeMk;
+		string namaMk;
+		int sks;
+	public:
+		void input() {
+			cout << "Kode MK :";
+			cin >> kodeMk;
+			cout << "Nama MK :";
+			cin >> namaMk;
+			cout << "SKS :";
+			cin >> sks;
+		}
+		void output() {
+			cout << "kode MK :" << kodeMk << endl;
+			cout << "Nama MK :" << namaMk << endl;
+			cout << "SKS :" << sks << endl;
+		}
 };
 
 int main() {
