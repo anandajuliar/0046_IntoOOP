88f7b25f23c85b61766b6e913ed476ebeda9d458
anandajuliar <anandajulia001@gmail.com>
2024-04-23 19:31:09 +0700

    int main 2

--- 0046_IntoOOP/0046_IntoOOP.cpp
+++ 0046_IntoOOP/0046_IntoOOP.cpp
@@ -39,4 +39,14 @@ int main() {
 	Mahasiswa mhs;
 	Matakuliah mk;
 
+	mhs.nama = "Budi";
+	mhs.umur = 20;
+	mhs.jurusan = "Teknik Informatika";
+	mhs.output();
+
+	mk.input();
+	mk.output();
+
+	return 0;
+}
 
