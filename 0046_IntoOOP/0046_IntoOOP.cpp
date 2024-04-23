16d858938611942b9ffd078f5b7f0728d816ee2a
anandajuliar <anandajulia001@gmail.com>
2024-04-23 19:28:01 +0700

    void input

--- 0046_IntoOOP/0046_IntoOOP.cpp
+++ 0046_IntoOOP/0046_IntoOOP.cpp
@@ -19,5 +19,14 @@ private:
 	string kodeMk;
 	string namaMk;
 	int sks;
+public:
+	void input() {
+		cout << "Kode MK :";
+		cin >> kodeMk;
+		cout << "Nama MK :";
+		cin >> namaMk;
+		cout << "SKS :";
+		cin >> sks;
+	}
 
 
